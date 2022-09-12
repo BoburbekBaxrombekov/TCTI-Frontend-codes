27e00d81498c00f4535cd6cc4c40130e96a18d5c
Boburbek <boburbaxrombekov@gmail.com>
2022-09-12 20:08:57 +0500

    editor of news fixed

--- src/components/edit_news/edit_news.jsx
+++ src/components/edit_news/edit_news.jsx
@@ -123,12 +123,12 @@ function EditNewsForm() {
                                                         plugins: [
                                                             'advlist autolink lists link image charmap print preview anchor',
                                                             'searchreplace visualblocks code fullscreen',
-                                                            'insertdatetime media table paste code help wordcount image | link | code'
+                                                            'insertdatetime media table paste code help wordcount image'
                                                         ],
                                                         toolbar: 'undo redo | formatselect | ' +
                                                             'bold italic backcolor | alignleft aligncenter ' +
                                                             'alignright alignjustify | bullist numlist outdent indent | ' +
-                                                            'removeformat | help | image | media',
+                                                            'removeformat | help | image | media | link | code',
                                                         content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                                                     }}
                                                 />
@@ -147,12 +147,12 @@ function EditNewsForm() {
                                                         plugins: [
                                                             'advlist autolink lists link image charmap print preview anchor',
                                                             'searchreplace visualblocks code fullscreen',
-                                                            'insertdatetime media table paste code help wordcount image | link | code'
+                                                            'insertdatetime media table paste code help wordcount image'
                                                         ],
                                                         toolbar: 'undo redo | formatselect | ' +
                                                             'bold italic backcolor | alignleft aligncenter ' +
                                                             'alignright alignjustify | bullist numlist outdent indent | ' +
-                                                            'removeformat | help | image | media',
+                                                            'removeformat | help | image | media | link | code',
                                                         content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                                                     }}
                                                 />
